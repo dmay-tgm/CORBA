822c8afd6733971baaabc7a3c47e90425240f9cb
root <dmay@student.tgm.ac.at>
2016-03-28 18:16:49 +0200

    Comment the client source and generate JavaDoc

--- client/src/cb/Client.java
+++ client/src/cb/Client.java
@@ -5,65 +5,85 @@ import org.omg.CORBA.*;
 import org.omg.PortableServer.*;
 
 /**
- * Client.java
- *
- *
+ * Simple client application that invokes remote methods using CORBA.
+ * 
  * Created: Mon Sep 3 19:28:34 2001
  *
- * @author Nicolas Noffke
+ * @author Nicolas Noffke, Daniel May
+ * @version 20160328.1
  */
+public class Client extends CallBackPOA {
+	/**
+	 * constructor
+	 */
+	public Client() {
+	}
 
-public class Client extends CallBackPOA
-{
-    public Client ()
-    {
-    }
+	/**
+	 * Method to receive messages from the server using the client side remote
+	 * object. Message will be printed out.
+	 *
+	 * @param message
+	 *            the received message
+	 */
+	public void call_back(String message) {
+		System.out.println("Client callback object received hello message >" + message + '<');
+	}
 
-    public void call_back (String message)
-    {
-        System.out.println ("Client callback object received hello message >"
-                + message + '<');
-    }
+	/**
+	 * Main function that starts the client application. Calls the remote method
+	 * one_time and registers 2 periodically invoked callbacks.
+	 *
+	 * @param args
+	 *            command line arguments
+	 * @throws Exception
+	 *             if the server or the naming service is not available
+	 */
+	public static void main(String[] args) throws Exception {
+		// ORB mit den Kommandazeilenargumenten initialisieren
+		org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, null);
 
-    public static void main (String[] args) throws Exception
-    {
-        org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init (args,null);
+		// Erhalten des RootContext des angegebenen Namingservices
+		org.omg.CORBA.Object o = orb.resolve_initial_references("NameService");
 
-	/* Erhalten des RootContext des angegebenen Namingservices */
-	org.omg.CORBA.Object o = orb.resolve_initial_references("NameService");
-			
-	/* Verwenden von NamingContextExt */
-	NamingContextExt rootContext = NamingContextExtHelper.narrow(o);
-			
-	/* Angeben des Pfades zum Echo Objekt */
-	NameComponent[] name = new NameComponent[2];
-	name[0] = new NameComponent("test","my_context");
-	name[1] = new NameComponent("Echo", "Object");
-        
-	//Testing
-	//org.omg.CORBA.Object o = orb.string_to_object ("IOR:010000001200000049444c3a63622f5365727665723a312e30000000010000000000000064000000010102000e0000003139322e3136382e312e3131390039b40e000000fe5259f85600001a6c000000000000000200000000000000080000000100000000545441010000001c00000001000000010001000100000001000105090101000100000009010100");
+		// Verwenden von NamingContextExt
+		NamingContextExt rootContext = NamingContextExtHelper.narrow(o);
 
-        Server server = ServerHelper.narrow (rootContext.resolve(name));
+		// Angeben des Pfades zum Echo Objekt
+		NameComponent[] name = new NameComponent[2];
+		name[0] = new NameComponent("test", "my_context");
+		name[1] = new NameComponent("Echo", "Object");
 
-	POA poa = POAHelper.narrow( orb.resolve_initial_references( "RootPOA" ));
-        
-        poa.the_POAManager ().activate ();
+		// aufloesen der Objektreferenz
+		Server server = ServerHelper.narrow(rootContext.resolve(name));
 
-        CallBack cb = CallBackHelper.narrow (poa.servant_to_reference (new Client ()));
-	
-	System.out.println("Executing one-time callback.");	
+		// RootPoa speichern
+		POA poa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
 
-        server.one_time (cb, "Hello! This is a test message.");
+		// aktivieren des Object Adapters
+		poa.the_POAManager().activate();
 
-	System.out.println("Register periodically callback that is called every 2 seconds.");	
+		// Client Objekt uber den RootPOA zur Verfuegung stellen
+		CallBack cb = CallBackHelper.narrow(poa.servant_to_reference(new Client()));
 
-        server.register (cb, "I'm the 2 seconds callback.", (short) 2);
+		System.out.println("Executing one-time callback.");
 
-	System.out.println("Register periodically callback that is called every 3 seconds.");	
-        server.register (cb, "This is another message (3 seconds).", (short) 3);
+		// invoking remote method
+		server.one_time(cb, "Hello! This is a test message.");
 
-	System.out.println("Press [ENTER] to shut down the server.");
-        while ( System.in.read() != '\n' );
-	server.shutdown();
-    }
-}// Client
+		System.out.println("Register periodically callback that is called every 2 seconds.");
+
+		// invoke remote method that calls the callback every 2 seconds.
+		server.register(cb, "I'm the 2 seconds callback.", (short) 2);
+
+		System.out.println("Register periodically callback that is called every 3 seconds.");
+		server.register(cb, "This is another message (3 seconds).", (short) 3);
+
+		System.out.println("Press [ENTER] to shut down the server.");
+		// waiting for keyboard interruption...
+		while (System.in.read() != '\n')
+			;
+		// invoke the remote method to shut down the server
+		server.shutdown();
+	}
+}
