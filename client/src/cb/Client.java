a5003d605fdc3919fdcbb0ee469371f51a82d37b
root <dmay@student.tgm.ac.at>
2016-03-28 11:48:07 +0200

    Testing periodically callback method

--- client/src/cb/Client.java
+++ client/src/cb/Client.java
@@ -39,20 +39,28 @@ public class Client extends CallBackPOA
 	NameComponent[] name = new NameComponent[2];
 	name[0] = new NameComponent("test","my_context");
 	name[1] = new NameComponent("Echo", "Object");
-        //org.omg.CORBA.Object o = orb.string_to_object ("IOR:010000001200000049444c3a63622f5365727665723a312e30000000010000000000000064000000010102000e0000003139322e3136382e312e3131390039b40e000000fe5259f85600001a6c000000000000000200000000000000080000000100000000545441010000001c00000001000000010001000100000001000105090101000100000009010100");
+        
+	//Testing
+	//org.omg.CORBA.Object o = orb.string_to_object ("IOR:010000001200000049444c3a63622f5365727665723a312e30000000010000000000000064000000010102000e0000003139322e3136382e312e3131390039b40e000000fe5259f85600001a6c000000000000000200000000000000080000000100000000545441010000001c00000001000000010001000100000001000105090101000100000009010100");
 
         Server server = ServerHelper.narrow (rootContext.resolve(name));
 
-        //POA root_poa = (POA) orb.resolve_initial_references ("RootPOA");
-
-	 POA poa = POAHelper.narrow( orb.resolve_initial_references( "RootPOA" ));
-        //bidir_poa.the_POAManager ().activate ();
+	POA poa = POAHelper.narrow( orb.resolve_initial_references( "RootPOA" ));
+        
         poa.the_POAManager ().activate ();
 
         CallBack cb = CallBackHelper.narrow (poa.servant_to_reference (new Client ()));
+	
+	System.out.println("Executing one-time callback.");	
 
         server.one_time (cb, "Hello! This is a test message.");
 
-        server.shutdown();
+	System.out.println("Executing periodically callback every 2 seconds.");	
+
+        server.register (cb, "This is a periodically occurring message.", (short) 2);
+
+	System.out.println("Press [ENTER] to shut down the server.");
+        while ( System.in.read() != '\n' );
+	server.shutdown();
     }
 }// Client
